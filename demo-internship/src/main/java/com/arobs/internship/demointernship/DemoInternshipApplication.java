34d3668ff5c2d0cfa5887bfaf873b369f969590f
Andreea Maier <andreea.maier@arobs.com>
2019-12-11 09:47:37 +0200

    first commit

---
+++ demo-internship/src/main/java/com/arobs/internship/demointernship/DemoInternshipApplication.java
@@ -0,0 +1,15 @@
+/*
+package com.arobs.internship.demointernship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInternshipApplication.class, args);
+	}
+
+}
+*/
